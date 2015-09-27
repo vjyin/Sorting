3ca59c2269d139b8e6f4835ea8c76fcf8dc59706
vjyin <vjyinbox@gmail.com>
2015-09-27 08:28:44 -0700

    bubbly sort

--- Sorting/src/BubbleSort.java
+++ Sorting/src/BubbleSort.java
@@ -1,7 +1,31 @@
 
 public class BubbleSort {
-	public static void main(String[] args) {
-		System.out.println("Bubble sort demo program");
+	
+	public static void main(String[] args) 
+	{		
+		int a[] = {1,3,2,10,18,4,3};
+		a = bubbleSort(a);
+		for (int i:a) 
+		{
+			System.out.print(i + " ");
+		}
 	}
-
+	
+	public static int[] bubbleSort(int[] input) 
+	{
+		for (int i=0;i<input.length;i++) 
+		{
+			for (int j = i; j<input.length;j++) 
+			{
+					if (input[i] > input[j])
+					{
+						//swap
+						int temp = input[i];
+						input[i]=input[j];
+						input[j] =temp;
+					}
+			}
+		}
+		return input;
+	}
 }
